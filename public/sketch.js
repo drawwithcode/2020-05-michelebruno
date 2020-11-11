26a44b0978a92f4e5000cceb59f21af75a8eabfa
michelebruno <bm.michelebruno@gmail.com>
2020-11-11 18:45:46 +0100

    Setup

---
+++ public/sketch.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-jsdoc */
+// Create a new connection using socket.io (imported in index.html)
+const socket = io();
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
