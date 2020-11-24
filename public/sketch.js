0d8683097960712b4bea9763c8b46eb16697acc3
michelebruno <bm.michelebruno@gmail.com>
2020-11-24 22:16:18 +0100

    commit

--- public/sketch.js
+++ public/sketch.js
@@ -8,26 +8,40 @@
  */
 const brushes = new Map();
 const socket = io();
-
 // The width of the player canvas, not the p5 one.
 const canvasWidth = 8000;
 const canvasHeight = canvasWidth * 0.737;
 let starshipIcon;
 let stars;
 let me;
+let myColor;
+let count= 0;
+
 const useFace = false;
+const status = [
+  DEAD = 'dead',
+  ALIVE = 'alive',
+];
+const positions = {
+  topLeft: [.05, .05],
+  topRight: [.95, .05],
+  bottomLeft: [.95, .05],
+  bottomRight: [.95, .95],
+};
+
+const messages = new Map();
 
 /**
  * Changes the direction of the brush and creates one if it's not there.
  */
-socket.on('brush', (id, x, y, dX, dY, col) => {
+socket.on('brush', (id, x, y, angle, col) => {
   if (!brushes.has(id)) {
     brushes.set(id, new StarShip({x, y, col}));
   }
 
   const brush = brushes.get(id);
 
-  brush.setDirection(dX, dY);
+  brush.setDirection(angle);
 
   brush.setPosition(createVector(x, y), undefined, true);
 });
@@ -37,6 +51,14 @@ socket.on('brush', (id, x, y, dX, dY, col) => {
  */
 socket.on('brush.leave', (id) => {
   brushes.get(id)?.die();
+});
+
+socket.on('color', (col) => {
+  if (me) {
+    return me.col = color(col);
+  } else {
+    myColor = col;
+  }
 });
 
 function preload() {
@@ -52,13 +74,21 @@ function setup() {
    * Creates a brush with a random position.
    * @type {StarShip}
    */
-  me = new StarShip({x: random(canvasWidth), y: random(canvasHeight), remote: 0});
+  me = new StarShip({
+    x: random(canvasWidth),
+    y: random(canvasHeight),
+    remote: false,
+    col: myColor,
+  });
+}
 
-  textAlign(RIGHT);
+function textInPos(t, pos) {
+  text(t, width * pos[0], height * pos[1]);
 }
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
+  push();
   background(3);
 
   translate(width / 2, height / 2);
@@ -68,10 +98,12 @@ function draw() {
     const dY = map(mouseY, 0, height, -height, height) / 15;
     me.setDirection(dX, dY);
   }
+
   /**
    * In order to have better speed control.
    */
   scale(.6);
+
   me.display();
 
   /**
@@ -80,11 +112,36 @@ function draw() {
   for (const [, brush] of brushes) {
     brush.display();
   }
-}
+  pop();
 
+
+  push();
+
+  fill('white');
+  textInPos('There are ' + (count -1 ) + ' spaceships around', positions.topLeft);
+
+  pop();
+
+  /**
+   * Warn about collision.
+   */
+  let collisions = 0;
+  brushes.forEach((b) => me.pos.dist(b.pos) < 200 && collisions++);
+
+
+  push();
+
+  if (collisions) {
+    fill('red');
+    textAlign(RIGHT);
+    textInPos('Risking collisions!', positions.topRight);
+  }
+  pop();
+}
 
 class StarShip {
   constructor({x = 0, y = 0, remote = true, col} = {}) {
+    this.status = ALIVE;
     this.pos = createVector(x, y);
     this.direction = createVector(0, 0);
 
@@ -92,13 +149,16 @@ class StarShip {
     this.remote = remote;
 
     this.col = col ? color(col) : color(random(255), random(255), random(255));
-
-    console.log('new brush', this.col, x, y);
+    count++;
   }
 
-  setDirection(x, y) {
-    this.direction = createVector(x, y);
-    this.direction.limit(10);
+  setDirection(xOrAngle, y) {
+    if (typeof y === 'undefined') {
+      this.direction = p5.Vector.fromAngle(xOrAngle);
+    } else {
+      this.direction = createVector(xOrAngle, y);
+      this.direction.limit(10);
+    }
   }
 
   setPosition(pos, add) {
@@ -109,6 +169,9 @@ class StarShip {
 
     this.pos = add ? pos.add(add) : pos;
 
+    /**
+     * Stops at the edges.
+     */
     if (this.pos.x > canvasWidth) {
       this.pos.x = canvasWidth;
     } else if (this.pos.x < 0) {
@@ -137,9 +200,7 @@ class StarShip {
         'brush',
         this.pos.x,
         this.pos.y,
-        this.direction.x,
-        this.direction.y,
-        this.col.toString(),
+        this.direction.heading(),
     );
   }
 
@@ -148,12 +209,13 @@ class StarShip {
    */
   checkHistoryLenght() {
     const maxLength = 200;
-    if (this.history.length > maxLength) {
+    while (this.history.length > maxLength) {
       this.history.shift();
     }
   }
 
   die() {
+    this.status = DEAD;
     this.setDirection(0, 0);
   }
 
@@ -164,27 +226,24 @@ class StarShip {
       translate(-me.pos.x, -me.pos.y);
 
       push();
-      noStroke();
-      fill(255);
-      rect(0, 0, canvasWidth, canvasHeight);
-
       image(stars, 0, 0, canvasWidth, canvasHeight);
       pop();
     }
 
-    push();
-    strokeWeight(40);
-
-    for (let i = 1; i < this.history.length; i++) {
-      const opacity = i/this.history.length * 255;
-      const col = this.col;
-      col.setAlpha(opacity);
-      stroke(col);
-      const current = this.history[i];
-      const to = this.history[i + 1];
-      to && line(to.x, to.y, current.x, current.y);
+    if (this.status === ALIVE) {
+      push();
+      strokeWeight(40);
+      for (let i = 1; i < this.history.length; i++) {
+        const opacity = i / this.history.length * 255;
+        const col = this.col;
+        col.setAlpha(opacity);
+        stroke(col);
+        const current = this.history[i];
+        const to = this.history[i + 1];
+        to && line(to.x, to.y, current.x, current.y);
+      }
+      pop();
     }
-    pop();
 
     if (this.history.length) {
       push();
--- server.js
+++ server.js
@@ -13,12 +13,27 @@ const io = require('socket.io')(server);
  * This script is very simple. It just emits back each position change.
  */
 io.on('connect', /** @param {Socket} socket */ (socket) => {
+  const color = getRandomColor();
+
+  socket.emit('color', color);
+
   socket.on('disconnect', () => {
     socket.broadcast.emit('brush.leave', socket.id);
   });
 
   // define what to do on different kind of messages
-  socket.on('brush', (x, y, posX, posY, col) => {
-    socket.broadcast.emit('brush', socket.id, x, y, posX, posY, col );
+  socket.on('brush', (x, y, angle) => {
+    socket.broadcast.emit('brush', socket.id, x, y, angle, color );
   });
 });
+
+
+// assign a diff color to each client
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
